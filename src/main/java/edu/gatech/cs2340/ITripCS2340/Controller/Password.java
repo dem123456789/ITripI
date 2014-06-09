00f872b22bb7723afb6c2939b1ccee42f61fecbd
DreamSoul <diao_em@hotmail.com>
2014-06-08 21:02:53 -0400

    Serialization setup
    Temporarily take the role of database
    Also enable Eclipse project file

--- src/main/java/edu/gatech/cs2340/ITripCS2340/Controller/Password.java
+++ src/main/java/edu/gatech/cs2340/ITripCS2340/Controller/Password.java
@@ -3,21 +3,16 @@ package edu.gatech.cs2340.ITripCS2340.Controller;
 /**
  * Created by Dreamsoul on 2014/6/8.
  */
-public class Account {
-    private String username;
+public class Password implements java.io.Serializable{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 5649792483568634900L;
     private String password;
 
-    public Account(String title, String task) {
-        this.username = username;
+    public Password(String password) {
         this.password = password;
-    }
-
-    public void setName(String username) {
-        this.username = username;
-    }
-
-    public String getUserName() {
-        return username;
     }
 
     public void setPassword(String password) {
@@ -27,10 +22,8 @@ public class Account {
     public String getPassword() {
         return password;
     }
-
     public int hashCode(){
         int result = 31;
-        result = 31 * result + (username != null ? username.hashCode() : 0);
         result = 31 * result + (password  != null ? password.hashCode() : 0);
         return result;
     }
@@ -41,8 +34,10 @@ public class Account {
         if((obj == null) || (obj.getClass() != this.getClass())) {
             return false;
         }
-        	// object must be Test at this point
-        Account test = (Account)obj;
-        return username.equals(test.username)  || (password != null && password.equals(test.password));
+        // object must be Test at this point
+        Password test = (Password)obj;
+        return password != null && password.equals(test.password);
     }
+
+
 }
